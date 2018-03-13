6a318f8673a5d159560c1e6c54af5b8ee24a5f5c
James Hobin <hobinjk@gmail.com>
2018-03-13 12:49:01 -0700

    Package properly

--- package.sh
+++ package.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+rm -rf node_modules
+npm install --production
 rm -f SHA256SUMS
-sha256sum *.json *.js LICENSE > SHA256SUMS
-npm pack
+sha256sum package.json *.js LICENSE > SHA256SUMS
+find node_modules -type f -exec sha256sum {} \; >> SHA256SUMS
+TARFILE=$(npm pack)
+tar xzf ${TARFILE}
+cp -r node_modules ./package
+tar czf ${TARFILE} package
+rm -rf package
+echo "Created ${TARFILE}"
