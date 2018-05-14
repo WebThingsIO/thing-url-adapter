62840c55a96c0ca1dd28dd32dd0818bd72de12ca
James Hobin <hobinjk@gmail.com>
2018-05-14 15:41:43 -0700

    Rate limit querying of known things
    Should fix #38

--- thing-url-adapter.js
+++ thing-url-adapter.js
@@ -364,6 +364,17 @@ class ThingURLAdapter extends Adapter {
 
     url = url.replace(/\/$/, '');
 
+    if (!this.knownUrls[url]) {
+      this.knownUrls[url] = {
+        digest: '',
+        timestamp: 0,
+      };
+    }
+
+    if (this.knownUrls[url].timestamp + 5000 > Date.now()) {
+      return;
+    }
+
     let res;
     try {
       res = await fetch(url, {headers: {Accept: 'application/json'}});
@@ -384,11 +395,14 @@ class ThingURLAdapter extends Adapter {
     hash.update(text);
     const dig = hash.digest('hex');
     let known = false;
-    if (this.knownUrls[url] === dig) {
+    if (this.knownUrls[url].digest === dig) {
       known = true;
     }
 
-    this.knownUrls[url] = dig;
+    this.knownUrls[url] = {
+      digest: dig,
+      timestamp: Date.now(),
+    };
 
     let data;
     try {
