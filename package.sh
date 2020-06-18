c27e24a7f118a0b1ac7e7265f27f88c9c077e97f
Michael Stegeman <michael@stegeman.me>
2020-06-18 11:48:06 -0800

    Update package.sh.

--- package.sh
+++ package.sh
@@ -1,14 +1,19 @@
-#!/bin/bash
+#!/bin/bash -e
 
 rm -rf node_modules
+
 npm install --production
-rm -f SHA256SUMS
-sha256sum manifest.json package.json *.js LICENSE README.md > SHA256SUMS
-rm -rf node_modules/.bin
-find node_modules -type f -exec sha256sum {} \; >> SHA256SUMS
-TARFILE=$(npm pack)
+
+shasum --algorithm 256 manifest.json package.json *.js LICENSE README.md > SHA256SUMS
+
+find node_modules \( -type f -o -type l \) -exec shasum --algorithm 256 {} \; >> SHA256SUMS
+
+TARFILE=`npm pack`
+
 tar xzf ${TARFILE}
 cp -r node_modules ./package
 tar czf ${TARFILE} package
-rm -rf package
-echo "Created ${TARFILE}"
+
+shasum --algorithm 256 ${TARFILE} > ${TARFILE}.sha256sum
+
+rm -rf SHA256SUMS package
